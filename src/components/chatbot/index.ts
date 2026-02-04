75bfb09a98207c9b196bada86e140517cc1b6c80
dunamis <63559221+Dunamisss@users.noreply.github.com>
2026-02-04 18:51:31 +0000

    new

--- src/components/chatbot/index.ts
+++
@@ -1,4 +0,0 @@
-export { ChatWidget } from "./ChatWidget";
-export { ChatWindow } from "./ChatWindow";
-export { ModelSelector } from "./ModelSelector";
-export { InstructionsModal } from "./InstructionsModal";
